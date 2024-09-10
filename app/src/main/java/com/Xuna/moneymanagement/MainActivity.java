e9fc1bac636d339f50ce7ca294c4a71aed6fcb8b
Xuan Phu <xuanphu1024@gmail.com>
2024-09-10 08:06:21 +0700

    feat(master): practise Java array

--- app/src/main/java/com/Xuna/moneymanagement/MainActivity.java
+++ app/src/main/java/com/Xuna/moneymanagement/MainActivity.java
@@ -1,12 +1,15 @@
 package com.Xuna.moneymanagement;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +23,36 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //Create array
+        ArrayList<String> arrayName = new ArrayList<>();
+
+        //Add element
+        arrayName.add("Java");
+        arrayName.add("JavaScript");
+        arrayName.add("TypeScript");
+
+        //Get length
+        Log.d("Xuna", "SIZE:" + arrayName.size());
+
+        //Get element
+        Log.d("Xuna", "ELEMENT:" + arrayName.get(1));
+
+        //Remove
+        arrayName.remove(1);
+
+        //Get length
+        Log.d("Xuna", "SIZE:" + arrayName.size());
+
+        //Get element
+        Log.d("Xuna", "ELEMENT:" + arrayName.get(1));
+
+        arrayName.add("Python");
+        arrayName.add("C++");
+
+        //Same forEach
+        for (String ten : arrayName) {
+            Log.d("Xuna", ten);
+        }
     }
 }
